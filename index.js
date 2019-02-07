d83a10068bf1ae4c8b27b391873e428daf88c78f
Andrei Korchagin <andrei.korchagin@gmail.com>
2019-02-07 14:15:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function getFirstSelector(selector) {
+  return querySelector(selector);
+}
 
+function nestedTarget() {
+  return querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const ranked = parseInt(document.querySelectorAll("ranked").innerHTML);
+}
+
+function deepestChild() {
+
+}
